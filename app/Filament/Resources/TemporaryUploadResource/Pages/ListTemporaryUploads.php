7388b814fc8010e66cd97377b9ffadcfedadb091
marco76tv <marco.sottana@gmail.com>
2026-01-21 13:39:35 +0100

    .

--- app/Filament/Resources/TemporaryUploadResource/Pages/ListTemporaryUploads.php
+++ app/Filament/Resources/TemporaryUploadResource/Pages/ListTemporaryUploads.php
@@ -24,6 +24,17 @@ class ListTemporaryUploads extends XotBaseListRecords
      */
     #[Override]
     public function getTableColumns(): array
+    
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ...array_map(
+                    fn($col) => is_object($col) ? $col : TextColumn::make($col),
+                    $this->getTableColumns()
+                )
+            ]);
+    }
     {
         return [
             'folder' => TextColumn::make('folder')
