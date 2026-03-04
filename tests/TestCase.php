4d4243da7906cc4fcd1484dfb62c89ab8263c1fe
marco76tv <marco.sottana@gmail.com>
2026-03-04 11:38:04 +0100

    .

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Media\Tests;
 
-use Modules\Media\Providers\MediaServiceProvider;
-use Modules\User\Providers\UserServiceProvider;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Modules\Xot\Tests\XotBaseTestCase;
 
 /**
