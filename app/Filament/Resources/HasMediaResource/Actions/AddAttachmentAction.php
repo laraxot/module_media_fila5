42b6be662168bdb85b2a2bffc05818852e88e2ab
marco76tv <marco.sottana@gmail.com>
2026-03-08 18:58:23 +0100

    .

--- app/Filament/Resources/HasMediaResource/Actions/AddAttachmentAction.php
+++ app/Filament/Resources/HasMediaResource/Actions/AddAttachmentAction.php
@@ -7,7 +7,6 @@ namespace Modules\Media\Filament\Resources\HasMediaResource\Actions;
 use Exception;
 use Filament\Forms\Components\BaseFileUpload;
 use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\RelationManagers\RelationManager;
 use Illuminate\Support\Str;
@@ -19,7 +18,7 @@ class AddAttachmentAction extends XotBaseAction
     protected function setUp(): void
     {
         parent::setUp();
-        $this->icon('heroicon-o-plus'
+        $this->icon('heroicon-o-plus')
             ->color('success')
             ->button()
             ->schema(fn (): array => static::getFormSchema(false))
@@ -42,38 +41,20 @@ class AddAttachmentAction extends XotBaseAction
     }
 
     /**
-     * @return array<int, Radio|TextInput|BaseFileUpload|FileUpload>
+     * @return array<int, TextInput|FileUpload>
      */
     public static function getFormSchema(bool $asset = true): array
     {
-        // Assert::string($disk = $asset ? config('xra.asset.attachments.disk.driver') : config('xra.operation.attachments.disk.driver'));
-        // Assert::isArray($file_types = $asset ? config('xra.asset.attachments.allowed_file_types') : config('xra.operation.attachments.allowed_file_types'));
         Assert::integer($max_size = config('media-library.max_file_size'));
 
         return [
             FileUpload::make('file')
                 ->hint(static::trans('fields.file_hint'))
                 ->storeFileNamesIn('original_file_name')
-                // ->disk($disk)
-                // ->acceptedFileTypes($file_types)
                 ->visibility('private')
                 ->maxSize($max_size)
                 ->required()
                 ->columnSpanFull(),
-            /*
-             * Radio::make('attachment_type')
-             * ->hiddenLabel()
-             * ->options(
-             * AttachmentTypeEnum::descriptionsByValue($asset ? AttachmentTypeEnum::cases() : AttachmentTypeEnum::operationCases()),
-             * )
-             * ->default(AttachmentTypeEnum::Image())
-             * ->columns(
-             * $asset ? \count(AttachmentTypeEnum::cases()) : \count(AttachmentTypeEnum::operationCases()),
-             * )
-             * ->required()
-             * ->columnSpanFull(),
-             */
-            // Radio::make('attachment_type')->columnSpanFull(),
             TextInput::make('name')
                 ->hint(static::trans('fields.name_hint'))
                 ->autocomplete(false)
@@ -86,7 +67,6 @@ class AddAttachmentAction extends XotBaseAction
     {
         $ownerRecord = $livewire->getOwnerRecord();
         $mediaCollection = $data['attachment_type'] ?? 'default';
-        // $mediaCollection = 'default';
 
         if (! method_exists($ownerRecord, 'addMediaFromDisk')) {
             throw new Exception('wip');
@@ -94,40 +74,20 @@ class AddAttachmentAction extends XotBaseAction
 
         $fileAdder = $ownerRecord->addMediaFromDisk($data['file'], config('attachment.upload.disk.driver'));
 
-        if ($fileAdder === null || ! is_object($fileAdder)) {
-            throw new Exception('Failed to add media from disk');
+        if (! is_object($fileAdder) || ! method_exists($fileAdder, 'setName') || ! method_exists($fileAdder, 'preservingOriginal') || ! method_exists($fileAdder, 'toMediaCollection')) {
+            throw new Exception('FileAdder error');
         }
 
-        if (! method_exists($fileAdder, 'setName') || ! method_exists($fileAdder, 'preservingOriginal') || ! method_exists($fileAdder, 'toMediaCollection')) {
-            throw new Exception('FileAdder does not have required methods');
+        $attachment = $fileAdder->setName($data['name'] ?? Str::beforeLast((string) ($data['original_file_name'] ?? ''), '.'))
+            ->preservingOriginal()
+            ->toMediaCollection($mediaCollection);
+
+        if (is_object($attachment) && method_exists($attachment, 'update')) {
+            $user_id = authId();
+            $attachment->update([
+                'created_by' => $user_id,
+                'updated_by' => $user_id,
+            ]);
         }
-
-        $fileAdderWithName = $fileAdder->setName($data['name'] ?? Str::beforeLast((string) ($data['original_file_name'] ?? ''), '.'));
-        if (! is_object($fileAdderWithName) || ! method_exists($fileAdderWithName, 'preservingOriginal')) {
-            throw new Exception('setName did not return valid object');
-        }
-
-        $fileAdderPreserving = $fileAdderWithName->preservingOriginal();
-        if (! is_object($fileAdderPreserving) || ! method_exists($fileAdderPreserving, 'toMediaCollection')) {
-            throw new Exception('preservingOriginal did not return valid object');
-        }
-
-        $attachment = $fileAdderPreserving->toMediaCollection($mediaCollection);
-
-        if (! is_object($attachment) || ! method_exists($attachment, 'update')) {
-            throw new Exception('toMediaCollection did not return valid object');
-        }
-
-        $user_id = authId();
-        $attachment->update([
-            'created_by' => $user_id,
-            'updated_by' => $user_id,
-        ]);
-
-        /*
-         * $attachment->created_by=$user_id;
-         * $attachment->created_by=$user_id;
-         * $attachment->save();
-         */
     }
 }
