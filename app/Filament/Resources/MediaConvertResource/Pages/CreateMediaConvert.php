8bdfa022f1edfd657203ab4e2a21252d0d63aaaf
marco76tv <marco.sottana@gmail.com>
2026-03-25 14:32:35 +0100

    .

--- app/Filament/Resources/MediaConvertResource/Pages/CreateMediaConvert.php
+++ app/Filament/Resources/MediaConvertResource/Pages/CreateMediaConvert.php
@@ -9,5 +9,5 @@ use Modules\Xot\Filament\Resources\Pages\XotBaseCreateRecord;
 
 class CreateMediaConvert extends XotBaseCreateRecord
 {
-    protected static string $resource = MediaConvertResource::class;
+    public static string $resource = MediaConvertResource::class;
 }
