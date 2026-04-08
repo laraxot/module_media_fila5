363f344733875700a740db0991aef59abdec01a0
marco76tv <marco.sottana@gmail.com>
2026-04-08 13:20:10 +0200

    .

--- app/Filament/Resources/TemporaryUploadResource/Pages/EditTemporaryUpload.php
+++ app/Filament/Resources/TemporaryUploadResource/Pages/EditTemporaryUpload.php
@@ -10,7 +10,7 @@ use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
 
 class EditTemporaryUpload extends XotBaseEditRecord
 {
-    public static string $resource = TemporaryUploadResource::class;
+    protected static string $resource = TemporaryUploadResource::class;
 
     /**
      * @return array<string, \Filament\Actions\Action|\Filament\Actions\ActionGroup>
