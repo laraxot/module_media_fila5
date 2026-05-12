c43c55d18b95ee7d70650a655b06edd447da91e5
marco76tv <marco.sottana@gmail.com>
2026-05-12 15:07:50 +0200

    .

--- app/Filament/Actions/AddAttachmentAction.php
+++ app/Filament/Actions/AddAttachmentAction.php
@@ -64,9 +64,6 @@ class AddAttachmentAction extends XotBaseAction
         ];
     }
 
-    /**
-     * @param array<string, mixed> $data
-     */
     public static function formHandlerCallback(RelationManager $livewire, array $data): void
     {
         $ownerRecord = $livewire->getOwnerRecord();
