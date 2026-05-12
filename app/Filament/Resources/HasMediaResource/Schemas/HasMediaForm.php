1968e955ab2e19d1ecca2ad266d557484436c8cc
marco76tv <marco.sottana@gmail.com>
2026-05-12 08:11:13 +0200

    .

--- app/Filament/Resources/HasMediaResource/Schemas/HasMediaForm.php
+++ app/Filament/Resources/HasMediaResource/Schemas/HasMediaForm.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\base_quaeris_fila5\var\www\_bases\base_quaeris_fila5\laravel\Modules\Media\app\Filament\Resources\HasMediaResource\Schemas;
+namespace Modules\Media\app\Filament\Resources\HasMediaResource\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
