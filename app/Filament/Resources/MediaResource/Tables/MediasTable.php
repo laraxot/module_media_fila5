5b237c41f05e6c3da1d78b57447507fcb1281765
Malebestia <malebestia@yahoo.it>
2026-05-26 12:01:42 +0200

    .

--- app/Filament/Resources/MediaResource/Tables/MediasTable.php
+++ app/Filament/Resources/MediaResource/Tables/MediasTable.php
@@ -17,6 +17,9 @@ class MediasTable extends XotBaseResourceTable
      */
     public function getTableColumns(): array
     {
+    /**
+     * @return array<int\|string, \Filament\Tables\Columns\Column>
+     */
         return [
             'id' => TextColumn::make('id')->searchable()->sortable(),
             'name' => TextColumn::make('name')->searchable()->sortable(),
