00dec747fe7c2879cb1133e84768b6d7e9536a46
Malebestia <malebestia@yahoo.it>
2026-05-26 11:59:39 +0200

    .

--- app/Services/SubtitleService.php
+++ app/Services/SubtitleService.php
@@ -40,7 +40,7 @@ class SubtitleService
     public static function getInstance(): self
     {
         if (! (self::$instance instanceof self)) {
-            self::$instance = new self;
+            self::$instance = new self();
         }
 
         return self::$instance;
