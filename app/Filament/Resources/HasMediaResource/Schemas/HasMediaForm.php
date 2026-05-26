984a7b63e5333e610152bee6bfd159d59f3b2eeb
Malebestia <malebestia@yahoo.it>
2026-05-26 12:01:56 +0200

    .

--- app/Filament/Resources/HasMediaResource/Schemas/HasMediaForm.php
+++ app/Filament/Resources/HasMediaResource/Schemas/HasMediaForm.php
@@ -11,7 +11,7 @@ use Modules\Xot\Filament\Resources\Schemas\XotBaseResourceForm;
 class HasMediaForm extends XotBaseResourceForm
 {
     /**
-     * @return array<int|string, \Filament\Forms\Components\Component>
+     * @return array<int|string, \Filament\Schemas\Components\Component>
      */
     public static function getFormSchema(): array
     {
