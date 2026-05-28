56e33cf96ec3ce9b1fef54e76b904950dc09722f
Malebestia <malebestia@yahoo.it>
2026-05-28 18:50:14 +0200

    .

--- docs/wiki/_archive/config.php
+++
@@ -1,7 +0,0 @@
----
-module: theme
-topic: config
-canonical: ../../../../../Themes/docs/shared-components/config.php
----
-
-See canonical documentation: ../../../../../Themes/docs/shared-components/config.php
