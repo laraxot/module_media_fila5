790c80d377ea3cf5ba967d44d1dc18424a424cde
Malebestia <malebestia@yahoo.it>
2026-06-09 09:16:05 +0200

    .

--- app/Filament/Clusters/Test/Pages/S3Test.php
+++ app/Filament/Clusters/Test/Pages/S3Test.php
@@ -35,8 +35,6 @@ use function Safe\unlink;
  *
  * @property array<string, mixed> $debugResults
  * @property \Filament\Schemas\Schema $form
- *
- * @phpstan-ignore-next-line
  */
 class S3Test extends XotBasePage
 {
