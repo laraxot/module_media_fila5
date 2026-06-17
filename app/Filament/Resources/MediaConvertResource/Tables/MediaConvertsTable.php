5e8f0e43a201630f33f816a03fcc48c21797e690
marco76tv <marco.sottana@gmail.com>
2026-06-17 15:33:40 +0200

    .

--- app/Filament/Resources/MediaConvertResource/Tables/MediaConvertsTable.php
+++ app/Filament/Resources/MediaConvertResource/Tables/MediaConvertsTable.php
@@ -4,7 +4,18 @@ declare(strict_types=1);
 
 namespace Modules\Media\Filament\Resources\MediaConvertResource\Tables;
 
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\BulkAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\BaseFilter;
+use Filament\Tables\Filters\SelectFilter;
+use Modules\Media\Actions\Video\ConvertVideoByMediaConvertAction;
+use Modules\Media\Datas\ConvertData;
+use Modules\Media\Models\MediaConvert;
 use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
 
 class MediaConvertsTable extends XotBaseResourceTable
@@ -18,6 +29,56 @@ class MediaConvertsTable extends XotBaseResourceTable
             'id' => TextColumn::make('id')->searchable()->sortable(),
             'created_at' => TextColumn::make('created_at')->dateTime(),
             'updated_at' => TextColumn::make('updated_at')->dateTime(),
+        ];
+    }
+
+    /**
+     * @return array<string, BaseFilter>
+     */
+    public function getTableFilters(): array
+    {
+        return [
+            'format' => SelectFilter::make('format')->options(MediaConvert::distinct()->pluck(
+                'format',
+                'format',
+            )->toArray(...)),
+            'codec_video' => SelectFilter::make('codec_video')->options(MediaConvert::distinct()->pluck(
+                'codec_video',
+                'codec_video',
+            )->toArray(...)),
+            'codec_audio' => SelectFilter::make('codec_audio')->options(MediaConvert::distinct()->pluck(
+                'codec_audio',
+                'codec_audio',
+            )->toArray(...)),
+        ];
+    }
+
+    /**
+     * @return array<string, Action|ActionGroup>
+     */
+    public function getTableActions(): array
+    {
+        return [
+            'view' => ViewAction::make(),
+            'edit' => EditAction::make(),
+            'convert' => Action::make('convert')->action(function (MediaConvert $record): void {
+                $record->update(['percentage' => 0]);
+                $data = ConvertData::from([
+                    'file' => $record->file,
+                    'disk' => $record->disk,
+                ]);
+                app(ConvertVideoByMediaConvertAction::class)->onQueue()->execute($data, $record);
+            }),
+        ];
+    }
+
+    /**
+     * @return array<string, BulkAction>
+     */
+    public function getTableBulkActions(): array
+    {
+        return [
+            'delete' => DeleteBulkAction::make(),
         ];
     }
 }
