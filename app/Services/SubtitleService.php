436237a6f8022c23ee2541dae0421edc33d13cf9
marco76tv <marco.sottana@gmail.com>
2026-06-30 09:26:20 +0200

    .

--- app/Services/SubtitleService.php
+++ app/Services/SubtitleService.php
@@ -43,7 +43,7 @@ class SubtitleService
     public static function getInstance(): self
     {
         if (! (self::$instance instanceof self)) {
-            self::$instance = new self;
+            self::$instance = new self();
         }
 
         return self::$instance;
