cf79af2acbe530eb08f60bbe14222494caabbf2a
marco76tv <marco.sottana@gmail.com>
2026-06-30 09:31:06 +0200

    .

--- app/Enums/AttachmentTypeEnum.php
+++ app/Enums/AttachmentTypeEnum.php
@@ -47,7 +47,7 @@ enum AttachmentTypeEnum: string implements HasLabel
 
     public function getTypeNote(): ?string
     {
-        $translationKey = sprintf('media::attachments.type_notes.%s', $value);
+        $translationKey = sprintf('media::attachments.type_notes.%s', $this->value);
         if (Lang::has($translationKey)) {
             return trans($translationKey);
         }
@@ -57,7 +57,7 @@ enum AttachmentTypeEnum: string implements HasLabel
 
     public function getLabel(): string
     {
-        return trans('media::attachments.types.'.$value);
+        return trans('media::attachments.types.'.$this->value);
     }
 
     // private static function translateBaseUniquePath(): string
