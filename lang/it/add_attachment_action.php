69f43146569951d1e9330f9036f8b9eeb2f926d7
marco76tv <marco.sottana@gmail.com>
2026-06-30 09:27:00 +0200

    .

--- lang/it/add_attachment_action.php
+++ lang/it/add_attachment_action.php
@@ -54,4 +54,27 @@ return [
             'label' => 'Elimina Add Attachment Action',
         ],
     ],
+    'plural_label' => 'Add Attachment Action (Plurale)',
+    'navigation' => [
+        'name' => 'Add Attachment Action',
+        'plural' => 'Add Attachment Action',
+        'group' => [
+            'name' => 'General',
+            'description' => 'General Settings',
+        ],
+        'label' => 'Add Attachment Action',
+        'sort' => 1,
+        'icon' => 'heroicon-o-collection',
+    ],
+    'actions' => [
+        'create' => [
+            'label' => 'Crea Add Attachment Action',
+        ],
+        'edit' => [
+            'label' => 'Modifica Add Attachment Action',
+        ],
+        'delete' => [
+            'label' => 'Elimina Add Attachment Action',
+        ],
+    ],
 ];
