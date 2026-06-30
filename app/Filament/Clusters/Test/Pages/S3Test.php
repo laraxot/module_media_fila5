cfe9a1721a79a4278ee195bc4cf8a87f543b51f6
marco76tv <marco.sottana@gmail.com>
2026-06-30 09:32:15 +0200

    .

--- app/Filament/Clusters/Test/Pages/S3Test.php
+++ app/Filament/Clusters/Test/Pages/S3Test.php
@@ -680,7 +680,7 @@ class S3Test extends XotBasePage
         try {
             $testData = 'This is a test file content for S3 upload/download test.';
             $testFileName = 'test-file-'.time().'.txt';
-            $localTestPath = storage_path('framework/cache/'.$testFileName);
+            $localTestPath = sys_get_temp_dir().'/'.$testFileName;
 
             // Create test file
             file_put_contents($localTestPath, $testData);
