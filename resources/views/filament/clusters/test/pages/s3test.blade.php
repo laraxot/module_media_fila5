8023a5ad7af630b25a0ca2fb37cc18cf5fc4ade2
marco76tv <marco.sottana@gmail.com>
2026-07-08 10:49:48 +0200

    .

--- resources/views/filament/clusters/test/pages/s3test.blade.php
+++ resources/views/filament/clusters/test/pages/s3test.blade.php
@@ -29,12 +29,12 @@ declare(strict_types=1);
         <form wire:submit="test01" class="space-y-6">
             {{-- Form principale --}}
             <div class="space-y-4">
-                {{ $form->render(
+                {{ $this->form->render() }}
             </div>
 
             {{-- Azioni del form --}}
             <div class="flex justify-end gap-4 pt-6 border-t border-gray-200 dark:border-gray-700">
-                @foreach ($getFormActions(
+                @foreach ($this->getFormActions() as $action)
                     {{ $action }}
                 @endforeach
             </div>
