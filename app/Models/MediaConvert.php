5ef844a3a5c2e12c01dae961766aeaebcadffbff
marco76tv <marco.sottana@gmail.com>
2026-07-08 08:59:28 +0200

    .

--- app/Models/MediaConvert.php
+++ app/Models/MediaConvert.php
@@ -95,65 +95,38 @@ class MediaConvert extends BaseModel
      */
     public function media(): BelongsTo
     {
-        return $this->belongsTo(Media::class);
+        return // @var mixed belongsTo(Media::class;
     }
 
     public function getDiskAttribute(?string $value): ?string
     {
-<<<<<<< HEAD
-        if ($this->media === null) {
+        if (// @var mixed media === null
             return null;
         }
 
-        return $this->media->disk;
-=======
-        if ($media === null)
-            return null;
-        }
-
-        return $media->disk;
->>>>>>> laraxot/dev
+        return // @var mixed media->disk;
     }
 
     public function getFileAttribute(?string $value): ?string
     {
-<<<<<<< HEAD
-        if ($this->media === null) {
+        if (// @var mixed media === null
             return null;
         }
 
-        return $this->media->path.'/'.$this->media->file_name;
-=======
-        if ($media === null)
-            return null;
-        }
-
-        return $media->path.'/'.$this->media->file_name;
->>>>>>> laraxot/dev
+        return // @var mixed media->path.'/'.$this->media->file_name;
     }
 
     public function getConvertedFileAttribute(?string $value): ?string
     {
-<<<<<<< HEAD
-        if ($this->media === null) {
+        if (// @var mixed media === null
             return null;
         }
-        $info = pathinfo($this->media->file_name);
-=======
-        if ($media === null)
-            return null;
-        }
-        $info = pathinfo($media->file_name);
->>>>>>> laraxot/dev
+        $info = pathinfo(// @var mixed media->file_name;
         // "dirname" => "."
         // "basename" => "20600550-uhd_3840_2160_30fps.mp4"
         // "extension" => "mp4"
         // "filename" => "20600550-uhd_3840_2160_30fps"
 
-<<<<<<< HEAD
-        return $this->media->path.'/conversions/'.$info['filename'].'_'.$this->id.'.'.$this->format;
-=======
-        return $media->path.'/conversions/'.$info['filename'].'_'.$this->id.'.'.$this->format;
->>>>>>> laraxot/dev
+        return // @var mixed media->path.'/conversions/'.$info['filename'].'_'.$this->id.'.'.$this->format;
     }
 }
