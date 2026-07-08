b86d08192b15bf11babe73cd229fbe0715ad8de2
marco76tv <marco.sottana@gmail.com>
2026-07-08 10:54:35 +0200

    .

--- tests/Filament/Resources/mediaconvertresourcetest.php
+++ tests/Filament/Resources/mediaconvertresourcetest.php
@@ -7,11 +7,7 @@ namespace Modules\Media\Tests\Filament\Resources;
 use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\TextInput;
 use Modules\Media\Filament\Resources\MediaConvertResource;
-<<<<<<< HEAD
 use Modules\Media\Tests\TestCase;
-=======
-use Tests\TestCase;
->>>>>>> laraxot/dev
 
 uses(TestCase::class);
 
