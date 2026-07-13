38842e361580c3975e40deb0298c608655169ffc
marco76tv <marco.sottana@gmail.com>
2026-07-13 21:06:09 +0200

    Ignore tests/AuditCoverage/

--- app/Actions/GetAttachmentsSchemaAction.php
+++ app/Actions/GetAttachmentsSchemaAction.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Modules\Media\Actions;
 
+use Spatie\QueueableAction\QueueableAction;
+
 use Filament\Forms\Components\FileUpload;
 use Filament\Schemas\Components\Utilities\Set;
 use Illuminate\Support\Arr;
@@ -11,6 +13,8 @@ use Webmozart\Assert\Assert;
 
 class GetAttachmentsSchemaAction
 {
+    use QueueableAction;
+
     /**
      * @param  array<string>  $attachments
      * @return array<FileUpload>
