3235c4506fdb603b021b5d9dd78114b4b81c077a
marco76tv <marco.sottana@gmail.com>
2026-07-27 12:30:12 +0200

    .

--- app/Models/TemporaryUpload.php
+++ app/Models/TemporaryUpload.php
@@ -7,6 +7,7 @@ namespace Modules\Media\Models;
 use Closure;
 use Exception;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Carbon;
@@ -26,7 +27,7 @@ use Webmozart\Assert\Assert;
 /**
  * Modules\Media\Models\TemporaryUpload.
  *
- * @property int $id
+ * @property string $id
  * @property string $session_id
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
@@ -74,8 +75,11 @@ class TemporaryUpload extends BaseModel implements HasMedia
     /** @phpstan-use HasXotFactory<TemporaryUploadFactory> */
     use HasXotFactory;
 
+    use HasUuids;
     use InteractsWithMedia;
     use MassPrunable;
+
+    public $incrementing = false;
 
     public static ?Closure $manipulatePreview = null;
 
