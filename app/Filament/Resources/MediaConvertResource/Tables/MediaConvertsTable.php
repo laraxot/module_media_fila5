ad5951b69899d1e34a72de5a6379201eef1c67c8
Marco Xot <marco76tv@hotmail.com>
2026-08-06 20:11:53 +0200

    .

--- app/Filament/Resources/MediaConvertResource/Tables/MediaConvertsTable.php
+++ app/Filament/Resources/MediaConvertResource/Tables/MediaConvertsTable.php
@@ -27,6 +27,9 @@ class MediaConvertsTable extends XotBaseResourceTable
      */
     public function getTableColumns(): array
     {
+    /**
+     * @return array<int\|string, \Filament\Tables\Columns\Column>
+     */
         return [
             'id' => TextColumn::make('id')->searchable()->sortable(),
             'created_at' => TextColumn::make('created_at')->dateTime(),
