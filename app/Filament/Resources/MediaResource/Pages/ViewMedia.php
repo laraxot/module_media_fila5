40e21e93c75811afa478e2031211311d1ca5bcef
Marco Xot <marco76tv@hotmail.com>
2026-08-06 20:02:24 +0200

    .

--- app/Filament/Resources/MediaResource/Pages/ViewMedia.php
+++ app/Filament/Resources/MediaResource/Pages/ViewMedia.php
@@ -96,7 +96,7 @@ class ViewMedia extends XotBaseViewRecord
     protected function getHeaderWidgets(): array
     {
         return [
-            ConvertWidget::make(['record' => $record]
+            ConvertWidget::make(['record' => $record])
         ];
     }
 }
