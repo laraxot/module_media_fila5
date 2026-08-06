e81c83b305867a3b907db72a21bf2f8ba215a4ed
Marco Xot <marco76tv@hotmail.com>
2026-08-06 20:13:21 +0200

    .

--- app/Filament/Clusters/Test/Pages/S3Test.php
+++ app/Filament/Clusters/Test/Pages/S3Test.php
@@ -35,6 +35,8 @@ use function Safe\unlink;
  * S3Test Page for AWS S3 testing and diagnostics.
  *
  * @property array<string, mixed> $debugResults
+ *
+ * @phpstan-ignore-next-line
  */
 class S3Test extends XotBasePage
 {
@@ -678,7 +680,7 @@ class S3Test extends XotBasePage
         try {
             $testData = 'This is a test file content for S3 upload/download test.';
             $testFileName = 'test-file-'.time().'.txt';
-            $localTestPath = storage_path('framework/cache/'.$testFileName);
+            $localTestPath = sys_get_temp_dir().'/'.$testFileName;
 
             // Create test file
             file_put_contents($localTestPath, $testData);
