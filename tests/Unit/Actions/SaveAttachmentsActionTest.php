81507e4aaea7559f608d4c1809a8b1c5445fecd3
Marco Xot <marco76tv@hotmail.com>
2026-08-06 20:33:29 +0200

    .

--- tests/Unit/Actions/SaveAttachmentsActionTest.php
+++ tests/Unit/Actions/SaveAttachmentsActionTest.php
@@ -14,6 +14,7 @@ use Spatie\MediaLibrary\MediaCollections\FileAdder;
 uses(TestCase::class);
 
 beforeEach(function (): void {
+    /** @var \Modules\Media\Tests\TestCase $this */
     Storage::fake('attachments');
 });
 
