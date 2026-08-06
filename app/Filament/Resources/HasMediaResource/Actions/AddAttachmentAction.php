271f6f270f031023b9e757e8332e11196f1a8d64
Marco Xot <marco76tv@hotmail.com>
2026-08-06 20:01:57 +0200

    .

--- app/Filament/Resources/HasMediaResource/Actions/AddAttachmentAction.php
+++ app/Filament/Resources/HasMediaResource/Actions/AddAttachmentAction.php
@@ -20,7 +20,7 @@ class AddAttachmentAction extends XotBaseAction
     protected function setUp(): void
     {
         parent::setUp();
-        $this->icon('heroicon-o-plus')
+        // @var mixed icon('heroicon-o-plus'
             ->color('success')
             ->button()
             ->schema(fn (): array => static::getFormSchema(false))
