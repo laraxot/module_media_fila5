f94eae4c2c48e98bfd3068b5e5fb3f74778222af
Marco Xot <marco76tv@hotmail.com>
2026-08-06 20:16:18 +0200

    .

--- database/migrations/2023_01_01_000000_create_temporary_uploads_table.php
+++ database/migrations/2023_01_01_000000_create_temporary_uploads_table.php
@@ -9,7 +9,7 @@ use Modules\Xot\Database\Migrations\XotBaseMigration;
 /*
  * Class CreateTemporaryUploadsTable.
  */
-return new class extends XotBaseMigration
+return new class() extends XotBaseMigration
 {
     protected ?string $model_class = TemporaryUpload::class;
 
