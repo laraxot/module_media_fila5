67e9c6e750e434e28fd3cde00ea2479d74db49da
Marco Xot <marco76tv@hotmail.com>
2026-09-09 16:28:20 +0200

    .

--- tests/Filament/Resources/mediaconvertresourcetest.php
+++ tests/Filament/Resources/mediaconvertresourcetest.php
@@ -2,17 +2,16 @@
 
 declare(strict_types=1);
 
-use Tests\TestCase;
 use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\TextInput;
 use Modules\Media\Filament\Resources\MediaConvertResource;
+use Tests\TestCase;
 
 uses(TestCase::class);
 
 test('get form schema returns expected components', function (): void {
-    $form = MediaConvertResource::getFormSchema();
+    $form = app(MediaConvertResource::class)->getFormSchema();
 
-    expect($form)->toBeArray();
     expect($form)->not->toBeEmpty();
 
     $componentClasses = array_map(get_class(...), $form);
