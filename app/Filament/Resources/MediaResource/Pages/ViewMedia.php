43637c38495bb74c5d83d0edd6558b6ae7d290ae
Marco Xot <marco.sottana@gmail.com>
2026-09-24 07:44:25 +0000

    .

---
+++ app/Filament/Resources/MediaResource/Pages/ViewMedia.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Media\Filament\Resources\MediaResource\Pages;
+
+use Filament\Actions\DeleteAction;
+use Modules\Media\Filament\Resources\MediaResource;
+use Modules\Media\Filament\Resources\MediaResource\Widgets\ConvertWidget;
+use Modules\Xot\Filament\Resources\Pages\XotBaseViewRecord;
+
+class ViewMedia extends XotBaseViewRecord
+{
+    protected static string $resource = MediaResource::class;
+
+    /**
+     * @return array<string, DeleteAction>
+     */
+    protected function getHeaderActions(): array
+    {
+        return [
+            'delete' => DeleteAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            ConvertWidget::make(['record' => $this->record]),
+        ];
+    }
+}
