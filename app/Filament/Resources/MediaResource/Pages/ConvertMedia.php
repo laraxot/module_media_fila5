928b25e80d868dc3b5d4e2f002014ac80677f6d4
Marco Xot <marco.sottana@gmail.com>
2026-09-24 10:46:45 +0000

    Enhance Media Resource Pages with Infolist Schema
    This commit introduces the `getInfolistSchema` method in both `ConvertMedia` and `ViewMedia` classes, leveraging the `MediaInfolist` schema for improved data representation. Additionally, several obsolete documentation files related to the Media module have been removed to streamline the project structure.
    
    # Conflicts:
    #	docs/archived/config.php

--- app/Filament/Resources/MediaResource/Pages/ConvertMedia.php
+++ app/Filament/Resources/MediaResource/Pages/ConvertMedia.php
@@ -6,8 +6,18 @@ namespace Modules\Media\Filament\Resources\MediaResource\Pages;
 
 use Modules\Media\Filament\Resources\MediaResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseViewRecord;
+use Modules\Media\Filament\Resources\MediaResource\Schemas\MediaInfolist;
 
 class ConvertMedia extends XotBaseViewRecord
 {
     protected static string $resource = MediaResource::class;
+
+    /**
+     * @return array<string, \Filament\Schemas\Components\Component>
+     */
+    #[\Override]
+    protected function getInfolistSchema(): array
+    {
+        return app(MediaInfolist::class)->getInfolistSchema();
+    }
 }
